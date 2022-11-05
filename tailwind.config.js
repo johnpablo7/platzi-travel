22bd9a85394a0082818f95269a1252221cbdd968
John Pablo Valverde <john.davis.2015.03@gmail.com>
2022-11-05 13:10:52 -0500

    add commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
+  content: ["./docs/index.html", "./src/**/*.{html,js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
     extend: {
